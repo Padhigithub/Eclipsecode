c64617231c3e47c6e6dbe2cda747b387059a0d70
Ravindra Padhi <ravindra.padhi@gmail.com>
2021-02-23 11:04:43 +0530

    Commiting feb 23

---
+++ TestIntro2021/src/Demo.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// create Driver object
+		// X driver = new X();
+	System.setProperty("webdriver.chrome.driver", "C:\\ChromDriver\\chromedriver.exe");	
+	WebDriver driver = new ChromeDriver();
+	driver.get("http://google.com");
+	System.out.println(driver.getTitle());
+	}
+	
+
+}
